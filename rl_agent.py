b2ee5a9ef36c8b86aa8a4eecb1ae4dd45215027f
SimonOuellette35 <s.ouellette1@gmail.com>
2019-02-08 16:23:55 -0500

    Revert "first commit"
    This reverts commit b49d413ceef25e3f8dc1c1c0f77cba5f4eec58d6.

--- rl_agent.py
+++ rl_agent.py
@@ -1,9 +1,7 @@
 import random
 import numpy as np
 from collections import deque
-import pymc3 as pm
-import matplotlib.pyplot as plt
-import theano.tensor as tt
+import xgboost as xgb
 
 class RLAgent:
 
@@ -15,9 +13,6 @@ class RLAgent:
         self.epsilon_min = 0.0
         self.epsilon_decay = 0.5
         self.model = None
-        self.MIN_VAL = 99.
-        self.MAX_VAL = 101.
-        self.D = int(((self.MAX_VAL - self.MIN_VAL) * 100) + 1)
 
     def remember(self, sar):
         self.memory.append(sar)
@@ -27,39 +22,51 @@ class RLAgent:
         if self.model is None:
             return 0.
 
-        # In the Q-table we return the Q value of the best action (including flat, if both long and short are negative),
-        # for the current state.
-        action_slice = self.Qmus_estimates[:, self.state_value_to_index(state)]
-        maxQ = np.max(action_slice, axis=0)
+        maxQ = -9999999.9
+        for a in range(self.action_size):
 
-        if maxQ < 0.:
-            return 0.       # we can just go flat, too
-        else:
-            return maxQ
+            np_state = np.reshape(state, [1, self.state_size])
+            np_action = np.reshape(a, [1, 1])
+
+            input = np.concatenate((np_state, np_action), axis=1)
+
+            input_xgb = xgb.DMatrix(input)
+            action_Q = self.model.predict(input_xgb)
+
+            if action_Q[0] > maxQ:
+                maxQ = action_Q[0]
+
+        return maxQ
 
     def act(self, state, use_explo=True):
         if use_explo:
-           if np.random.rand() < self.epsilon or self.model is None:
-               return random.randrange(self.action_size)
+            if np.random.rand() < self.epsilon or self.model is None:
+                return random.randrange(self.action_size)
 
-        action_slice = self.Qmus_estimates[:, self.state_value_to_index(state)]
-        best_Q = np.max(action_slice)
-        idx_best_action = np.argmax(action_slice)
+        act_values=[]
+        for a in range(self.action_size):
 
-        if best_Q < 0:
-            return 1.
-        elif idx_best_action == 0:
-            return 0.
-        else:
-            return 2.
+            np_state = np.reshape(state, [1, self.state_size])
+            np_action = np.reshape(a, [1, 1])
 
-    def state_value_to_index(self, s):
-        # map spread value to an index...
-        idx = (s[0] - self.MIN_VAL) * 100
+            input = np.concatenate((np_state, np_action), axis=1)
 
-        return int(idx)
+            input_xgb = xgb.DMatrix(input)
+            action_Q = self.model.predict(input_xgb)
+
+            act_values.append(action_Q)
+
+        return np.argmax(act_values)  # returns optimal action
 
     def replay(self):
+        # specify parameters via map
+        max_depth = 6
+        num_round = 3
+
+        gamma = 0.05
+        eta = 0.75
+
+        param = {'max_depth': max_depth, 'eta': eta, 'silent': 1, 'gamma': gamma, 'objective': 'reg:linear'}
 
         mem = np.array(self.memory)
 
@@ -67,74 +74,13 @@ class RLAgent:
         actions = np.reshape(mem[:, self.state_size], [-1, 1])
         rewards = mem[:, -1]
 
-        full_tensor = []
-        s_short = []
-        r_short = []
-        for t in range(len(states)):
+        # train an xgb model on current memory
+        input_training = np.concatenate((states, actions), axis=1)
 
-            idx = self.state_value_to_index(states[t])
+        training_data = xgb.DMatrix(input_training,
+                                    label=rewards)
 
-            if actions[t] == 0:
-                full_tensor.append(np.array([0, idx, rewards[t]]))
-                s_short.append(idx)
-                r_short.append(rewards[t])
-            elif actions[t] == 2:
-                full_tensor.append(np.array([1, idx, rewards[t]]))
-
-        # plt.scatter(s_short, r_short)
-        # plt.show()
-
-        # qvalues = [N x 3]
-        # 1 - action index (0 - short, 1 - long)
-        # 2 - state index
-        # 3 - reward
-        qvalues = np.array(full_tensor)
-        print "full_tensor shape = ", qvalues.shape
-
-
-        # TODO: why does the more trained domain have a higher variance? Shouldn't increased certainty correlate with
-        # lower variance???
-
-        # TODO: why is it not learning anything above X = 125?
-
-        with pm.Model() as self.model:
-
-            def likelihood(Qmus, Qsds):
-
-                def _logp(value):
-                    idx0 = tt.cast(value[:, 0], dtype='int8')
-                    idx1 = tt.cast(value[:, 1], dtype='int8')
-                    return pm.Normal.dist(mu=Qmus[idx0, idx1], sd=np.exp(Qsds[idx0, idx1])).logp(value[:, 2])
-                    #return pm.Normal.dist(mu=Qmus[idx0, idx1], sd=Qsds[idx0, idx1]).logp(value[:, 2])
-
-                return _logp
-
-            Qmus = pm.Normal('Qmus', mu=0., sd=1., shape=[2, self.D])
-            Qsds = pm.Normal('Qsds', mu=-1., sd=.01, shape=[2, self.D])
-            # Qmus = pm.Uniform('Qmus', lower=-1., upper=1., shape=[2, self.D])
-            # Qsds = pm.Uniform('Qsds', lower=0.01, upper=0.2, shape=[2, self.D])
-
-            pm.DensityDist('Qtable',
-                           likelihood(Qmus, Qsds),
-                           observed=qvalues)
-
-            mean_field = pm.fit(n=8000, method='advi', obj_optimizer=pm.adam(learning_rate=.15))
-            self.trace = mean_field.sample(5000)
-
-            #self.trace = pm.sample(1000, tune=1000)
-
-        self.Qmus_estimates = np.mean(self.trace['Qmus'], axis=0)
-        self.Qsds_estimates = np.median(self.trace['Qsds'], axis=0)
-
-        fig, (ax1, ax2) = plt.subplots(1, 2)
-
-        ax1.plot(self.Qmus_estimates[0])
-        ax1.set_title("Q-values for Short action")
-
-        ax2.plot(self.Qsds_estimates[0])
-        ax2.set_title("Certainties for Short action")
-
-        plt.show()
+        self.model = xgb.train(param, training_data, num_round)
 
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
